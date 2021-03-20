3802b55a71be563369812ddc5d13cee0c30612d9
Petr Abakumov <petr-abakumow@yandex.ru>
2021-03-20 15:26:44 +0300

    feat: сверстать попап с телефоном

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('Привет, Мир!');
+
+const phoneButton = document.querySelector('.contacts-block__button_tel');
+const popupPhone = document.querySelector('.popup-phone');
+
+function popupPhoneOpened() {
+    popupPhone.classList.add('popup-phone_opened');
+}
+
+phoneButton.addEventListener('click', () => {
+    popupPhoneOpened();
+})
